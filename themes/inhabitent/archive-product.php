1ab4413632bc14dd0d84d0bfa2d3ab5f4a731d0b
Sorrelljs <[sjsohota@gmail.com]>
2020-01-21 19:14:43 -0800

    new widget made

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -16,6 +16,7 @@
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				//	the_archive_img('<img class = "page-img">)'
 				?>
 			</header><!-- .page-header -->
 
